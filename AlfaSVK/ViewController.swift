26aad55a465733a87448d88d07ccb80259a7b75d
Vagan Galstian <vagamagajaga@yandex.ru>
2023-05-04 19:01:35 +0700

    Initial Commit

---
+++ AlfaSVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlfaSVK
+//
+//  Created by Vagan Galstian on 04.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
